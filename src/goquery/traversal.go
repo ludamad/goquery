59c4f601d5f134352a5f5ffed2c7ae2532396209
Adam <domuradical@gmail.com>
2013-10-17 23:05:03 -0400

    modify traversal.go

--- src/goquery/traversal.go
+++ src/goquery/traversal.go
@@ -18,14 +18,15 @@ type astState struct {
 
 type AstContext struct {
 	*astState
-	functionContext *ast.FuncDecl
+	FunctionContext *ast.FuncDecl
+	ParentNodes []ast.Node
 }
 
 type AstVisitor interface {
 	// Return whether to navigate to the children nodes
 	Visit(node ast.Node, ac AstContext) bool
 }
-
+ 
 // Bridge to go's AST traversal API:
 type visitorImpl struct {
 	ast     AstContext
@@ -36,12 +37,20 @@ func (v visitorImpl) Visit(node ast.Node) ast.Visitor {
 	funcNode, _ := node.(*ast.FuncDecl)
 	// If we are in a function node, set the function context
 	if funcNode != nil {
-		v.ast.functionContext = funcNode
+		v.ast.FunctionContext = funcNode
 	}
 	visitChildren := v.visitor.Visit(node, v.ast)
 	if visitChildren {
+		// Push node
+		v.ast.ParentNodes = append(v.ast.ParentNodes, node)
 		return v
 	} else {
+		// Pop node
+		v.ast.ParentNodes = v.ast.ParentNodes[:len(v.ast.ParentNodes)-1]
 		return nil
 	}
+}
+
+func AstWalk(visitor AstVisitor, string[] files ) {
+
 }
