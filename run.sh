b9d953df1063522a05ac60c2a6d9ee8e921b5f45
Adam <domuradical@gmail.com>
2013-10-27 21:52:44 -0400

    Bypass makefile for now

--- run.sh
+++ run.sh
@@ -3,6 +3,4 @@ go get "github.com/stevedonovan/luar"
 go install "github.com/stevedonovan/luar"
 export GOPATH=$GOPATH:$(pwd)
 go install go-future/types # TODO: Properly depend on this package. This is built here so that the typechecker can find it.
-make
-
-./bin/main
+go run src/main.go
