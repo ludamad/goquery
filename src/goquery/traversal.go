b902f71a5df0b3965f3dd711f7e5037cfcde534a
Adam <domuradical@gmail.com>
2013-10-27 21:42:39 -0400

    Revert "No real change, make it compile again"
    This reverts commit 16cbc1b44d831702fc537e2c43ab95a4f0a36edc.

--- src/goquery/traversal.go
+++ src/goquery/traversal.go
@@ -16,41 +16,41 @@ type astState struct {
 	currentScope map[*ast.Object]bool
 }
 
-//type AstContext struct {
-//	*astState
-//	FunctionContext *ast.FuncDecl
-//	ParentNodes []ast.Node
-//}
-//
-//type AstVisitor interface {
-//	// Return whether to navigate to the children nodes
-//	Visit(node ast.Node, ac AstContext) bool
-//}
-// 
-//// Bridge to go's AST traversal API:
-//type visitorImpl struct {
-//	ast     AstContext
-//	visitor AstVisitor
-//}
-//
-//func (v visitorImpl) Visit(node ast.Node) ast.Visitor {
-//	funcNode, _ := node.(*ast.FuncDecl)
-//	// If we are in a function node, set the function context
-//	if funcNode != nil {
-//		v.ast.FunctionContext = funcNode
-//	}
-//	visitChildren := v.visitor.Visit(node, v.ast)
-//	if visitChildren {
-//		// Push node
-//		v.ast.ParentNodes = append(v.ast.ParentNodes, node)
-//		return v
-//	} else {
-//		// Pop node
-//		v.ast.ParentNodes = v.ast.ParentNodes[:len(v.ast.ParentNodes)-1]
-//		return nil
-//	}
-//}
-//
-//func AstWalk(visitor AstVisitor, string[] files ) {
-//
-//}
+type AstContext struct {
+	*astState
+	FunctionContext *ast.FuncDecl
+	ParentNodes []ast.Node
+}
+
+type AstVisitor interface {
+	// Return whether to navigate to the children nodes
+	Visit(node ast.Node, ac AstContext) bool
+}
+ 
+// Bridge to go's AST traversal API:
+type visitorImpl struct {
+	ast     AstContext
+	visitor AstVisitor
+}
+
+func (v visitorImpl) Visit(node ast.Node) ast.Visitor {
+	funcNode, _ := node.(*ast.FuncDecl)
+	// If we are in a function node, set the function context
+	if funcNode != nil {
+		v.ast.FunctionContext = funcNode
+	}
+	visitChildren := v.visitor.Visit(node, v.ast)
+	if visitChildren {
+		// Push node
+		v.ast.ParentNodes = append(v.ast.ParentNodes, node)
+		return v
+	} else {
+		// Pop node
+		v.ast.ParentNodes = v.ast.ParentNodes[:len(v.ast.ParentNodes)-1]
+		return nil
+	}
+}
+
+func AstWalk(visitor AstVisitor, string[] files ) {
+
+}
